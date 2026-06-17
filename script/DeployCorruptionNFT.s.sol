744c0766976e5443fff06dfc028a12623daaddfc
kasoutarou666 <akihiro4689@gmail.com>
2026-06-17 22:22:17 +0900

    feat: deploy CorruptionNFT contract to Base mainnet

---
+++ script/DeployCorruptionNFT.s.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {Script} from "forge-std/Script.sol";
+import {CorruptionNFT} from "../src/CorruptionNFT.sol";
+
+contract DeployCorruptionNFT is Script {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+        
+        CorruptionNFT nft = new CorruptionNFT();
+        
+        vm.stopBroadcast();
+    }
+}
